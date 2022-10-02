cd6bea23a68e152581c8019750b17e06ad07ed3a
Lioz Akirav <lioz.akirav@gmail.com>
2022-10-03 00:06:45 +0300

    counting days

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import tweepy as twitter
 import keys
-from datetime import datetime
+from datetime import *
 
 auth = twitter.OAuthHandler(keys.API_KEY, keys.API_KEY_SECRET)
 auth.set_access_token(keys.ACCESS_TOKEN, keys.SECRET_ACESS_TOKEN)
-api = twitter.API(auth)
+API = twitter.API(auth)
 
-def twitter_bot()
+def main():
+    # 08/29/22 - 84 days
+    date_format = "%m/%d/%Y"
+    quit_date = datetime.strptime('06/06/2022', date_format)
+    current_date = datetime.now()
+    delta = (current_date - quit_date).days
+    days_since_quit = str(delta)
+    tweet = 'עברו ' + days_since_quit + ' ימים מאז הפעם האחרונה שעישנתי 💪🏽'
+    API.update_status(tweet)
+
+    
+if __name__ == "__main__":
+    main()
